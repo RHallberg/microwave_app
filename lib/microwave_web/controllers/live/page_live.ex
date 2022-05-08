2d4b32ffe9156070b7005b3d7f10dc9ada3c5345
RHallberg <richal@bredband2.se>
2022-05-08 11:33:54 +0200

    handling of no microwave available case

--- lib/microwave_web/controllers/live/page_live.ex
+++ lib/microwave_web/controllers/live/page_live.ex
@@ -35,12 +35,31 @@ defmodule MicrowaveWeb.PageLive do
 
   @impl true
   def handle_event("submit_time", %{"microwave" => %{"time" => time}}, socket = %{assigns: %{microwaves: microwaves}}) do
-    Microwave.MicrowaveTimer.start_timer(microwaves["foo"].pid, Integer.parse(time) |> elem(0))
-    {:noreply, assign(socket, microwaves: %{microwaves | "foo" => %{microwaves["foo"] | state: :busy} })}
+    available = find_available(microwaves)
+    start_if_available(socket, Integer.parse(time) |> elem(0), find_available(microwaves))
   end
 
   @impl true
   def handle_info(%{event: "microwave_done", payload: id}, socket = %{assigns: %{microwaves: microwaves}} ) do
     {:noreply, assign(socket, microwaves: %{microwaves | id => %{microwaves[id] | state: :available} })}
+  end
+
+  defp start_if_available(socket, _time, nil) do
+    Logger.info("No microwave available!")
+    {:noreply, socket}
+  end
+
+  defp start_if_available(socket = %{assigns: %{microwaves: microwaves}}, time, id) do
+    Microwave.MicrowaveTimer.start_timer(microwaves[id].pid, time)
+    {:noreply, assign(socket, microwaves: %{microwaves | id => %{microwaves[id] | state: :busy} })}
+  end
+
+
+  defp find_available(microwaves) do
+    available = Enum.find(microwaves, fn m -> m
+      |> elem(1)
+      |> Map.fetch(:state)
+      |> elem(1) == :available end)
+    unless is_nil(available), do: (available |> elem(0)), else: available
   end
 end
