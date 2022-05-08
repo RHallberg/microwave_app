5764babe26f3444513d6422d007be06d8d36488e
RHallberg <richal@bredband2.se>
2022-05-08 10:52:29 +0200

    State showing on microwave

--- lib/microwave_web/controllers/live/page_live.ex
+++ lib/microwave_web/controllers/live/page_live.ex
@@ -4,7 +4,12 @@ defmodule MicrowaveWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, pid} = Microwave.MicrowaveTimer.start_link(%{topic: "st4", id: "foo"})
+
+    microwave_id = "foo"
+    microwaves = %{microwave_id => %{
+      pid: Microwave.MicrowaveTimer.start_link(%{topic: "st4", id: microwave_id}) |> elem(1),
+      state: :available
+    }}
     if connected?(socket) do
       MicrowaveWeb.Endpoint.subscribe("st4")
     end
@@ -13,7 +18,7 @@ defmodule MicrowaveWeb.PageLive do
         socket,
         topic: "st4",
         time: 0,
-        microwave: pid
+        microwaves: microwaves
     )}
   end
 
@@ -24,19 +29,18 @@ defmodule MicrowaveWeb.PageLive do
 
   @impl true
   def handle_event("decrement_time", _params, socket = %{assigns: %{time: time}}) do
-    new_time = unless time == 0, do: time - 11, else: 0
+    new_time = unless time == 0, do: time - 1, else: 0
     {:noreply, assign(socket, time: new_time)}
   end
 
   @impl true
-  def handle_event("submit_time", %{"microwave" => %{"time" => time}}, socket = %{assigns: %{microwave: microwave}}) do
-    Microwave.MicrowaveTimer.start_timer(microwave, Integer.parse(time) |> elem(0))
-    {:noreply, socket}
+  def handle_event("submit_time", %{"microwave" => %{"time" => time}}, socket = %{assigns: %{microwaves: microwaves}}) do
+    Microwave.MicrowaveTimer.start_timer(microwaves["foo"].pid, Integer.parse(time) |> elem(0))
+    {:noreply, assign(socket, microwaves: %{microwaves | "foo" => %{microwaves["foo"] | state: :busy} })}
   end
 
   @impl true
-  def handle_info(%{event: "microwave_done", payload: id}, socket ) do
-    Logger.info("Microwave #{id} done!")
-    {:noreply, socket}
+  def handle_info(%{event: "microwave_done", payload: id}, socket = %{assigns: %{microwaves: microwaves}} ) do
+    {:noreply, assign(socket, microwaves: %{microwaves | id => %{microwaves[id] | state: :available} })}
   end
 end
